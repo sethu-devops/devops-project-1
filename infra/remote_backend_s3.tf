9657633286f59c56d3c49695d52fc90067f97dc6
sethu-devops <sethuaws@outlook.com>
2024-08-13 17:46:03 +0530

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "dev-proj-1-remote-state-bucket-124"
     key    = "devops-project-1/terraform.tfstate"
-    region = "eu-central-1"
+    region = "ca-central-1"
   }
 }
